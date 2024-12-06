ab86297cb06944402ebb7e38b6a33eb1662795aa
kazz2607 <kairu2607@gmail.com>
2024-12-06 22:15:32 +0700

    Bai 160

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,19 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
-mix.less('resources/less/style.less', 'public/css')
-.less('resources/less/admin.less', 'public/css')
-.sass('resources/scss/app.scss', 'public/css/style.css')
-.stylus('resources/stylus/app.styl', 'public/css');
-// .styles('resources/css/style.css', 'public/css');
-
-
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
