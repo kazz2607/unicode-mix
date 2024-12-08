7db125338c2c2e5771f3aa204dc1226a897ef4bc
kazz2607 <kairu2607@gmail.com>
2024-12-08 21:55:55 +0700

    Bai 165

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css')
-//     .sourceMaps();
-
-mix.js('resources/js/custom.js', 'public/js');
-// .js('resources/js/custom.js', 'public/js');
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css').version();
